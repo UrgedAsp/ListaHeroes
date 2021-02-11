5c66a573bd19106c7e37bc0c0f72f81c58476aa9
Sebastian <johanncastillo96@gmail.com>
2021-02-10 20:43:02 -0500

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadoresModule } from './contadores/contadores.module';
 import { DbzModule } from './dbz/dbz.module';
-import { PruebaComponent } from './prueba/prueba.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PruebaComponent
+    AppComponent
   ],
   imports: [
     HeroesModule,
